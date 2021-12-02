d5b19e2a47a47805eee312d9f394cc2d692c3a0b
Bearior <zazamheemook@gmail.com>
2021-12-02 20:40:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,38 +8,38 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [pictureUrl, setPictureUrl] = useState();
-  const [idToken, setIdToken] = useState("");
-  const [displayName, setDisplayName] = useState("");
-  const [statusMessage, setStatusMessage] = useState("");
-  const [userId, setUserId] = useState("");
+  // const [pictureUrl, setPictureUrl] = useState();
+  // const [idToken, setIdToken] = useState("");
+  // const [displayName, setDisplayName] = useState("");
+  // const [statusMessage, setStatusMessage] = useState("");
+  // const [userId, setUserId] = useState("");
 
-  const initLine = () => {
-    liff.init({ liffId: '1656554390-BDkoRm7V' }, () => {
-      if (liff.isLoggedIn()) {
-        runApp();
-      } else {
-        liff.login();
-      }
-    }, err => console.error(err));
-  }
+  // const initLine = () => {
+  //   liff.init({ liffId: '1656554390-dJV0qrQR' }, () => {
+  //     if (liff.isLoggedIn()) {
+  //       runApp();
+  //     } else {
+  //       liff.login();
+  //     }
+  //   }, err => console.error(err));
+  // }
 
 
-  const runApp = () => {
-    const idToken = liff.getIDToken();
-    setIdToken(idToken);
-    liff.getProfile().then(profile => {
-      console.log(profile);
-      setDisplayName(profile.displayName);
-      setPictureUrl(profile.pictureUrl);
-      setStatusMessage(profile.statusMessage);
-      setUserId(profile.userId);
-    }).catch(err => console.error(err));
-  }
+  // const runApp = () => {
+  //   const idToken = liff.getIDToken();
+  //   setIdToken(idToken);
+  //   liff.getProfile().then(profile => {
+  //     console.log(profile);
+  //     setDisplayName(profile.displayName);
+  //     setPictureUrl(profile.pictureUrl);
+  //     setStatusMessage(profile.statusMessage);
+  //     setUserId(profile.userId);
+  //   }).catch(err => console.error(err));
+  // }
 
-  useEffect(() => {
-    initLine();
-  }, []);
+  // useEffect(() => {
+  //   initLine();
+  // }, []);
 
 
   return (
