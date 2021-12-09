acea38f64c63a45b6d4b3740632a3a81cbb70931
Bearior <zazamheemook@gmail.com>
2021-12-09 11:13:41 +0700

    w

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Contact from "./components/Contact";
+import Contact from "./Contact";
 import "./app.css";
 // import liff from '@line/liff';
 // import { useEffect, useState } from 'react';
